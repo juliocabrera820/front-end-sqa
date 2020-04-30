246d41dd143d6ecde661c1ae689f0fccc25c3078
juliocabrera820 <juliocabrera820@gmail.com>
2020-04-30 11:25:02 -0500

    componente header

--- src/componentes/header.js
+++ src/componentes/header.js
@@ -1,7 +1,10 @@
 import React from 'react';
 
-const header = () => {
-    return (<div><h2>header</h2></div>);
-}
+const header = (props) => (<nav className="navbar navbar-light bg-dark">
+  <a className="navbar-brand text-white">SGH</a>
+  <form className="form-inline">
+    <button className="btn btn-outline-danger my-2 my-sm-0" type="submit">Cerrar sesion</button>
+  </form>
+</nav>);
 
 export default header;
