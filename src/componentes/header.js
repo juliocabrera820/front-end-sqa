d0014ce69859c902f9d3d58d5b9b855dfb6e15b9
juliocabrera820 <juliocabrera820@gmail.com>
2020-04-30 20:23:01 -0500

    header

--- src/componentes/header.js
+++ src/componentes/header.js
@@ -1,10 +1,25 @@
-import React from 'react';
+import React from "react";
+import { useDispatch } from "react-redux";
+import { withRouter } from "react-router-dom";
 
-const header = (props) => (<nav className="navbar navbar-light bg-dark">
-  <a className="navbar-brand text-white">SGH</a>
-  <form className="form-inline">
-    <button className="btn btn-outline-danger my-2 my-sm-0" type="submit">Cerrar sesion</button>
-  </form>
-</nav>);
+const Header = (props) => {
+  const { history } = props;
+  const dispatch = useDispatch();
+  const salir = () => {
+    dispatch({ type: "SET_USUARIO", payload: "No hay usuario" });
+    history.push("/");
+  };
 
-export default header;
+  return (
+    <nav className="navbar navbar-light bg-dark">
+      <a className="navbar-brand text-white">SGH</a>
+      <form className="form-inline">
+        <button className="btn btn-outline-danger my-2 my-sm-0" onClick={salir}>
+          Cerrar sesion
+        </button>
+      </form>
+    </nav>
+  );
+};
+
+export default withRouter(Header);
