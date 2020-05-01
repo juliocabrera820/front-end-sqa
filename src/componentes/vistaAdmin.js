c6cb3b40ec2efc62a3ca18352c6133d2ae9ed551
juliocabrera820 <juliocabrera820@gmail.com>
2020-04-30 20:25:42 -0500

    Redireccionar y cerrar sesion

--- src/componentes/vistaAdmin.js
+++ src/componentes/vistaAdmin.js
@@ -1,7 +1,22 @@
-import React from 'react';
+import React, { useEffect } from "react";
+import { useSelector } from "react-redux";
+import { withRouter } from "react-router-dom";
+import Header from "./header";
 
-const vistaAdmin = () => {
-    return (<div><h2>Componente vista del admin</h2></div>);
-}
+const VistaAdmin = (props) => {
+  const { history } = props;
+  const estado = useSelector((state) => state);
+  useEffect(() => {
+    if (estado.Usuario === "No hay usuario") {
+      history.push("/");
+    }
+  }, []);
+  return (
+    <div className="container-fluid">
+      <Header />
+      <h2 className="text-center">Vista admin</h2>
+    </div>
+  );
+};
 
-export default vistaAdmin;
+export default withRouter(VistaAdmin);
