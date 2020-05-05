84d4641fdd98f4e7d3bb5554d5cc82213dddbada
juliocabrera820 <juliocabrera820@gmail.com>
2020-05-05 16:25:51 -0500

    redux-persist

--- src/App.js
+++ src/App.js
@@ -4,26 +4,18 @@ import vistaAlumno from "./componentes/VistaAlumno";
 import vistaAdmin from "./componentes/vistaAdmin";
 import vistaprofes from "./componentes/vistaProfes";
 import NoFound from "./componentes/NoFound";
-import {
-  Route,
-  BrowserRouter as Router,
-  Switch,
-} from "react-router-dom";
-import { Provider } from "react-redux";
-import { store } from "./redux/store";
+import { Route, BrowserRouter as Router, Switch } from "react-router-dom";
 
 const App = () => (
-  <Provider store={store}>
-    <Router>
-      <Switch>
-        <Route exact path="/" component={login} />
-        <Route exact path="/Maestro" component={vistaprofes} />
-        <Route exact path="/Alumno" component={vistaAlumno} />
-        <Route exact path="/Administrador" component={vistaAdmin} />
-        <Route  component={NoFound} />
-      </Switch>
-    </Router>
-  </Provider>
+  <Router>
+    <Switch>
+      <Route exact path="/" component={login} />
+      <Route exact path="/Maestro" component={vistaprofes} />
+      <Route exact path="/Alumno" component={vistaAlumno} />
+      <Route exact path="/Administrador" component={vistaAdmin} />
+      <Route component={NoFound} />
+    </Switch>
+  </Router>
 );
 
 export default App;
