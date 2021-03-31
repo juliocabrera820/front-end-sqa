0c08709bb4ac11d4c793e8c3291b166f60e46a65
juliocabrera820 <juliocabrera820@gmail.com>
2021-03-31 14:42:15 -0600

    crear schema para la pagina de iniciar sesion

---
+++ src/schemas/loginSchema.js
@@ -0,0 +1,6 @@
+import * as yup from 'yup';
+
+export const loginSchema = yup.object().shape({
+  username: yup.string().trim().required('El nombre de usuario es un campo requerido'),
+  password: yup.string().trim().required('La contraseña es un campo requerido'),
+});
