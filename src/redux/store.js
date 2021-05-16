70a075204534b354e5fe6378fdf4b7ec02bb3689
Ariel May <aemd199711@gmail.com>
2021-05-15 20:14:39 -0500

    agregar reductores y evitar almacenar el horario d d unn usuario

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,15 +1,22 @@
-import { createStore, applyMiddleware, compose } from "redux";
+import { createStore, applyMiddleware, compose, combineReducers } from "redux";
 import { persistStore, persistReducer } from "redux-persist";
 import thunk from "redux-thunk";
 import storage from "redux-persist/lib/storage";
-import reducer from "./reducers/sessionReducer";
+import SessionReducer from "./reducers/sessionReducer";
+import ScheduleReducer from "./reducers/scheduleReducer";
 
 const persistConfig = {
   key: "root",
+  blacklist: ["schedule"],
   storage,
 };
 
-const persistRed = persistReducer(persistConfig, reducer);
+const rootReducer = combineReducers({
+  auth: SessionReducer,
+  schedule: ScheduleReducer,
+});
+
+const persistRed = persistReducer(persistConfig, rootReducer);
 export const store = createStore(
   persistRed,
   compose(
