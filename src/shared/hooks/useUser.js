5988bea8d8c7e456219ea2fa6f60eab7cc48e5e7
Ariel May <aemd199711@gmail.com>
2021-05-15 20:21:53 -0500

    obtener es estado dde la sesion

--- src/shared/hooks/useUser.js
+++ src/shared/hooks/useUser.js
@@ -1,6 +1,8 @@
 import { useSelector } from "react-redux";
 
 export const useUser = (history) => {
-  const { currentUser, isLoading, redirectTo } = useSelector((state) => state);
+  const { currentUser, isLoading, redirectTo } = useSelector(
+    (state) => state.auth
+  );
   return { currentUser, isLoading, redirectTo };
 };
